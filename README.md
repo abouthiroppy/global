bab305295476d001ab84a2e81e34adea9683b28a
abouthiroppy <git@about-hiroppy.com>
2016-11-27 22:03:45 +0900

    Add list and install commands

---
+++ README.md
@@ -0,0 +1,26 @@
+# global
+
+My global commands.
+
+## Install
+```
+$ npm i -g @about_hiroppy/global
+```
+
+## Usage
+```shell
+$ global list # show global commands
+$ global install # install default packages
+```
+
+## Packages
+- babel-cli
+- electron
+- emoj
+- fixpack
+- honyaku
+- my-dish
+- np
+- placehold-cli
+- rgb-hex-cli
+- yarn
--- cli.js
+++ cli.js
@@ -4,10 +4,28 @@
 
 const spawn = require('cross-spawn');
 
-const args = [
-  'list',
-  '-g',
-  '--depth=0'
-];
+const commands = process.argv.slice(2);
 
-spawn('npm', args, {stdio: 'inherit'});
+if (commands.length === 0) {
+  console.log('Usage: global <list | install>');
+  process.exit(0);
+}
+
+if (commands[0] === 'list') {
+  const args = [
+    'list',
+    '-g',
+    '--depth=0'
+  ];
+
+  spawn('npm', args, {stdio: 'inherit'});
+} 
+
+if (commands[0] === 'install') {
+  const args = [
+    'run',
+    'setup'
+  ];
+
+  spawn('npm', args, {stdio: 'inherit'});
+} 
